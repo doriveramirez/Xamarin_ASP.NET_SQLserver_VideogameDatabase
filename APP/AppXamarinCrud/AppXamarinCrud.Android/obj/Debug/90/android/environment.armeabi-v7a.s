5a1733546ffb76cded6903d387eb988977e6c3c1
doriveramirez <44475416+doriveramirez@users.noreply.github.com>
2019-12-13 17:20:16 +0000

    0.8d

--- APP/AppXamarinCrud/AppXamarinCrud.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ APP/AppXamarinCrud/AppXamarinCrud.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"aa630311-944c-46cc-a8cd-ad9148639f28"
+	.asciz	"d61b2ace-e716-4c59-adea-a8d813f3b339"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
