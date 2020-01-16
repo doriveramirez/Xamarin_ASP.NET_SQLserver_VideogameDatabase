26fc14eb0c0012b907c76143f7230ece3471ad32
doriveramirez <44475416+doriveramirez@users.noreply.github.com>
2020-01-16 10:57:13 +0000

    test

---
+++ API/MVCCrudAPI/ReportViews/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
+namespace winRdlc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.visor.RefreshReport();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            string p1 = t1.Text.Trim();
+            string p2 = dt1.Value.ToShortDateString();
+            string p3 = dt2.Value.ToShortDateString();
+            dtsEjemplo dtsE = new dtsEjemplo();
+
+
+            #region "CREA TABLA DE EJEMPLO"
+
+            DataTable tabla = new DataTable();
+            DataColumn col = new DataColumn("d1");
+            col.DataType = System.Type.GetType("System.String");
+            tabla.Columns.Add(col);
+            col = new DataColumn("d2");
+            col.DataType = System.Type.GetType("System.String");
+            tabla.Columns.Add(col);
+            col = new DataColumn("d3");
+            col.DataType = System.Type.GetType("System.String");
+            tabla.Columns.Add(col);
+            col = new DataColumn("d4");
+            col.DataType = System.Type.GetType("System.String");
+            tabla.Columns.Add(col);
+            col = new DataColumn("d5");
+            col.DataType = System.Type.GetType("System.String");
+            tabla.Columns.Add(col);
+
+            #endregion
+
+
+            for (int i = 0; i < 10;i++)
+            {
+                DataRow linea = tabla.NewRow();
+                linea["d1"] = "Linea " + (i+1).ToString();
+                linea["d2"] = "Linea " + (i + 1).ToString();
+                linea["d3"] = "Linea " + (i + 1).ToString();
+                linea["d4"] = "Linea " + (i + 1).ToString();
+                linea["d5"] = "Linea " + (i + 1).ToString();
+
+                tabla.Rows.Add(linea);
+            }
+
+            dtsE.Tables.RemoveAt(0);    //Eliminamos la tabla que crea por defecto
+            dtsE.Tables.Add(tabla);     //Añadimos la tabla que acabamos de crear
+
+            dtsEjemploBindingSource.DataSource = dtsE;
+            dtsEjemploBindingSource.DataMember = "Table1";
+
+            //Array que contendrá los parámetros
+            ReportParameter[] parameters = new ReportParameter[3];
+            //Establecemos el valor de los parámetros
+            parameters[0] = new ReportParameter("p1", p1);
+            parameters[1] = new ReportParameter("p2", p2);
+            parameters[2] = new ReportParameter("p3", p3);
+            //Pasamos el array de los parámetros al ReportViewer
+            visor.LocalReport.SetParameters(parameters);
+
+            visor.Width = 1032;
+            visor.Height = 600;
+            visor.Visible = true;
+            visor.RefreshReport();
+
+        }
+
+        private void t1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtsEjemploBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ API/MVCCrudAPI/ReportViews/Form1.designer.cs
@@ -0,0 +1,146 @@
+﻿namespace winRdlc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dtsEjemploBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.dt1 = new System.Windows.Forms.DateTimePicker();
+            this.dt2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtsEjemploBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtsEjemploBindingSource
+            // 
+            this.dtsEjemploBindingSource.DataMember = "dtsEjemplo";
+            this.dtsEjemploBindingSource.CurrentChanged += new System.EventHandler(this.dtsEjemploBindingSource_CurrentChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Título";
+            // 
+            // t1
+            // 
+            this.t1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.t1.Location = new System.Drawing.Point(50, 6);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(329, 20);
+            this.t1.TabIndex = 2;
+            this.t1.Text = "TÍTULO DE PRUEBA";
+            this.t1.TextChanged += new System.EventHandler(this.t1_TextChanged);
+            // 
+            // dt1
+            // 
+            this.dt1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dt1.Location = new System.Drawing.Point(438, 6);
+            this.dt1.Name = "dt1";
+            this.dt1.Size = new System.Drawing.Size(99, 20);
+            this.dt1.TabIndex = 3;
+            // 
+            // dt2
+            // 
+            this.dt2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dt2.Location = new System.Drawing.Point(584, 6);
+            this.dt2.Name = "dt2";
+            this.dt2.Size = new System.Drawing.Size(99, 20);
+            this.dt2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Desde";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(543, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hasta";
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(721, 7);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 7;
+            this.b1.Text = "Imprimir";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 490);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dt2);
+            this.Controls.Add(this.dt1);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtsEjemploBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer visor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.DateTimePicker dt1;
+        private System.Windows.Forms.DateTimePicker dt2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.BindingSource dtsEjemploBindingSource;
+        private dtsEjemplo dtsEjemplo;
+    }
+}
+
